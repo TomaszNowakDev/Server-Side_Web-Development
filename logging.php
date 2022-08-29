10cfbce7d2b0e122c4da094f329706bb33f12258
Tomasz Nowak <tomeknew1@gmail.com>
2022-08-29 21:26:09 +0100

    Checking for first time users added.

--- logging.php
+++ logging.php
@@ -15,11 +15,26 @@ session_start();
 <body>
     Log in to your account. <br/><hr/>
     <?php
+
+    if (!((isset($_POST['username'])) && (isset($_POST['password']))))
+    {
+        // it is our first time around.
+        // nothing has been entered yet.
         echo "<form action=\"logging.php\" method=\"POST\">";
         echo "Username: <br/><input name=\"username\" type=\"text\" required \"/><br/>";
         echo "Password: <br/><input name=\"password\" type=\"password\" required \"/><br/><hr/>";
         echo "<input type=\"reset\" value=\"Reset\"> ";
         echo "<input type=\"submit\" value=\"Log in\"/>";
+    }
+    else
+    {
+        // it does exist already. Some something has been sent to be processed
+        $theUsername = $_POST['username'];
+        $thePassword = $_POST['password'];
+        $theUsername = htmlentities($theUsername, ENT_QUOTES, "Utf-8");
+        $thePassword = htmlentities($thePassword, ENT_QUOTES, "Utf-8");
+
+    }
 
     ?>
     </form>
