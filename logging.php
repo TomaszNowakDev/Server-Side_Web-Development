f726fe7dcd229c5a1917145d8405c7682c9694f6
Tomasz Nowak <tomeknew1@gmail.com>
2022-08-29 20:31:40 +0100

    Initial commit, start of the project.

---
+++ logging.php
@@ -0,0 +1,30 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Blogs</title>
+    <meta name="Blog of the authors of famous rock songs" content="Blogs" />
+
+</head>
+
+<body>
+    Log in to your account. <br/><hr/>
+    <?php
+
+
+    ?>
+    </form>
+    <hr/>
+<br/>
+<br/>
+<footer>Tomasz Nowak | Server-Side Web Development Project</footer>
+<br/>
+<br/>
+</body>
+
+</html>
