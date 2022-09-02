eaa8887d9e01df6a643182b41bef72920a1a4446
Tomasz Nowak <tomeknew1@gmail.com>
2022-09-02 11:11:10 +0100

    Postcontent, title, style session varaible set.

--- createpost.php
+++ createpost.php
@@ -25,21 +25,31 @@ if (!((isset($_SESSION['username']))&&(isset($_SESSION['password']))))
 }
 else
 {
-	echo "<p>Welcome ".$_SESSION['username'].'! [ <a href="logout.php">Logout!</a> ]</p>';
+	echo "<p>Welcome ".$_SESSION['acc']." ".$_SESSION['username'].'! [ <a href="logout.php">Logout!</a> ]</p>';
 
+	if((isset($_POST['title'])) && (isset($_POST['postcontent'])))
+	{
+		$theTitle = $_POST['title'];
+		$thePostcontent = $_POST['postcontent'];
+		$theStyle = $_POST['style'];
+		$theTitle = htmlentities($theTitle, ENT_QUOTES, "Utf-8");
+		$thePostcontent = htmlentities($thePostcontent, ENT_QUOTES, "Utf-8");
 
-	// it is first time around on this page
-	echo "<form action=\"createpost.php\" method=\"POST\">";
-	echo "Title: <input name=\"title\" type=\"text\" required \"/><br/><br/>";
-	echo "Post content: <textarea name=\"postcontent\" type=\"text\" cols=\"50\" rows=\"12\" required \"></textarea><br/><br/>";
-	echo "<input type=\"radio\" name=\"style\" value=\"maroon\">Maroon ";
-	echo "<input type=\"radio\" name=\"style\" value=\"navy\">Navy ";
-	echo "<input type=\"radio\" name=\"style\" value=\"darkgreen\">Green ";
-	echo "<input type=\"radio\" name=\"style\" value=\"purple\">Purple ";
-	echo "<input type=\"radio\" name=\"style\" value=\"brown\">Brown<br/><br/>";
-	echo "Written by ".$_SESSION['username'].".<br/><br/>";
-	echo "<input type=\"submit\" value=\"Publish now\"/>";
-
+	}
+	else
+	{
+		// it is first time around on this page
+		echo "<form action=\"createpost.php\" method=\"POST\">";
+		echo "Title: <input name=\"title\" type=\"text\" required \"/><br/><br/>";
+		echo "Post content: <textarea name=\"postcontent\" type=\"text\" cols=\"50\" rows=\"12\" required \"></textarea><br/><br/>";
+		echo "<input type=\"radio\" name=\"style\" value=\"maroon\">Maroon ";
+		echo "<input type=\"radio\" name=\"style\" value=\"navy\">Navy ";
+		echo "<input type=\"radio\" name=\"style\" value=\"darkgreen\">Green ";
+		echo "<input type=\"radio\" name=\"style\" value=\"purple\">Purple ";
+		echo "<input type=\"radio\" name=\"style\" value=\"brown\">Brown<br/><br/>";
+		echo "Written by ".$_SESSION['username'].".<br/><br/>";
+		echo "<input type=\"submit\" value=\"Publish now\"/>";
+	}
 }
 
 ?>
